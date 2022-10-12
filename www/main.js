6400582762456be4f2f12adcef669ffc916eaa49
Alexander Senger <senger.alexander@yahoo.de>
2022-10-13 01:14:12 +0200

    extend example

--- www/main.js
+++ www/main.js
@@ -1,25 +1,74 @@
-'use strict';
+/* eslint-disable import/extensions,no-unused-vars */
+
 // import ScrollObserver from './ScrollObserver.js';
-import {ScrollObserver} from './ScrollObserver.js';
+import ScrollTracker from './ScrollTracker.js';
+
+const getCurrentScrollPercentage = () => {
+  const st = 'scrollTop';
+  const sh = 'scrollHeight';
+
+  const h = document.documentElement;
+  const b = document.body;
+
+  const depth = (h[sh] || b[sh]) - h.clientHeight;
+  const scrollTop = h[st] || b[st];
+
+  return depth === 0 ? 0 : Math.round((scrollTop / depth) * 100);
+};
+
+const messageElement = document.querySelector('.message');
+const percentageElement = document.querySelector('.percentage');
+
+const setUI = (depthInfo) => {
+  if (depthInfo.state === 'page loaded') {
+    messageElement.textContent = depthInfo.state;
+    percentageElement.textContent = `${getCurrentScrollPercentage()} %`;
+    return;
+  }
+
+  messageElement.textContent = depthInfo.state;
+  percentageElement.textContent = `${depthInfo.depth} %`;
+};
 
 function main() {
-  const scrollObserver = new ScrollObserver({
-    thresholds: '0.25',
-    triggerOnce: true,
-    triggerPrevious: true,
-    showMarkers: true,
-  });
-
   window.dataLayer = window.dataLayer || [];
 
-  scrollObserver.observe([25, 50], (depth) => {
+  // const scrollObserver = new ScrollObserver({
+  //   thresholds: '0.25',
+  //   // triggerOnce: true,
+  //   triggerPrevious: true,
+  //   showMarkers: true,
+  // });
+
+  // // setUI({ state: 'page loaded' });
+
+  // scrollObserver.observe([25, 50], (depthInfo) => {
+  //   setUI(depthInfo);
+  //   window.dataLayer.push({
+  //     depth: depthInfo.depth,
+  //   });
+  // });
+  // scrollObserver.observe(75, (depthInfo) => {
+  //   setUI(depthInfo);
+  //   window.dataLayer.push({
+  //     depth: depthInfo.depth,
+  //   });
+  // });
+
+  const scrollTracker = new ScrollTracker({
+    triggerOnce: true,
+    triggerPrevious: true,
+  });
+  scrollTracker.observe(20, (depthInfo) => {
+    setUI(depthInfo);
     window.dataLayer.push({
-      depth,
+      depth: depthInfo.depth,
     });
   });
-  scrollObserver.observe(75, (depth) => {
+  scrollTracker.observe(60, (depthInfo) => {
+    setUI(depthInfo);
     window.dataLayer.push({
-      depth,
+      depth: depthInfo.depth,
     });
   });
 }
@@ -29,6 +78,6 @@ function contentLoadedHandler(evt) {
   main.call(this);
 }
 
-if (typeof window != 'undefined') {
+if (typeof window !== 'undefined') {
   window.addEventListener('DOMContentLoaded', contentLoadedHandler);
 } else main();
